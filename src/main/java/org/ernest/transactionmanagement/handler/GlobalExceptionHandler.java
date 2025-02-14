213d78297da6289f7ff31d17c0dab03913ad2e1b
ernest <chaiyx1994@163.com>
2025-02-14 16:58:13 +0800

    处理字段校验异常格式,调一下日志级别

--- src/main/java/org/ernest/transactionmanagement/handler/GlobalExceptionHandler.java
+++ src/main/java/org/ernest/transactionmanagement/handler/GlobalExceptionHandler.java
@@ -8,10 +8,16 @@ import org.ernest.transactionmanagement.response.Response;
 import org.ernest.transactionmanagement.response.ResponseFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.context.request.WebRequest;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Slf4j
 @ControllerAdvice
@@ -23,6 +29,32 @@ public class GlobalExceptionHandler {
     public ResponseEntity<Response> handleRrException(BusenessException e) {
         log.error("Handling BusenessException Error Message: {}", e.getMessage(), e);
         return new ResponseEntity<>(ResponseFactory.error(e.getMessage()), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity<Response> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+
+        // 将 errors 拼接成指定格式的字符串
+        StringBuilder errorMessageBuilder = new StringBuilder();
+        errors.forEach((fieldName, errorMessage) -> {
+            errorMessageBuilder.append(fieldName)
+                    .append(": ")
+                    .append(errorMessage)
+                    .append(", ");
+        });
+
+        // 去掉最后一个多余的逗号和空格
+        if (errorMessageBuilder.length() > 0) {
+            errorMessageBuilder.setLength(errorMessageBuilder.length() - 2);
+        }
+        return new ResponseEntity<>(ResponseFactory.error(errorMessageBuilder.toString()), HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(Exception.class)
